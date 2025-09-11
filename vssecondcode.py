664fa5fceeae428fa23aeff958f6ad1da62e55ca
ASHWINPATIDAR18 <ashwinpatidar2000@gmail.com>
2025-09-11 16:17:07 +0530

    Create vssecondcode.py

---
+++ vssecondcode.py
@@ -0,0 +1,34 @@
+'''x=5
+if x>0:
+    print("Yes x is positive")
+    print("Bye Bye")
+print("Hello")'''
+
+
+'''x=-5
+if x>0:
+    print("Yes x is positive")
+    print("Bye Bye")
+print("Hello")'''
+
+
+'''x=int(input("Enter a number:"))
+if x>0:
+    print(x, "is positive")
+elif x<0:
+    print(x, "is negative")
+elif x==0:
+    print(x, "is neither negative nor positive")'''
+
+
+#python prog to find max of two numbers
+p=int(input("Enter first number:"))
+q=int(input("Enter second number:"))
+if p>q:
+    print(p,"is greater")
+elif q>p:
+    print(q,"is greater")
+else:
+    print("Both are equal")
+
+
