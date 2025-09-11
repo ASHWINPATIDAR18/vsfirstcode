1022335dc6d1b6478b2ceccefbb279037c66c7a9
ASHWINPATIDAR18 <ashwinpatidar2000@gmail.com>
2025-09-11 11:24:36 +0530

    Initial commit

---
+++ vsfirst.py
@@ -0,0 +1,48 @@
+'''print("Hello Everyone")
+str=input("Enter Something:")
+print(str)
+
+
+x=55
+y=7.1
+z="H"
+w=5+7j
+l1=[10,20,6.4,40,70]  
+t1="A","B","C"  
+s1={1,2,3,4,5,6}   
+q=int(p)'''
+
+'''p="123"
+r=float(p)
+
+import keyword
+print(len(keyword.kwlist))
+print(type(r))'''
+
+
+'''import math
+x=math.factorial(6)
+print(x)
+y=math.lcm(8,10)
+print(y)'''
+
+#import turtle
+#z=turtle.Screen()
+#z.setup(800,800)
+
+'''x=input("Enter first number:")
+print()
+print()
+print()
+print(end='   ')
+y=input("Enter second number:")
+print(x,y)'''
+
+#x=int(input("Enter first number:"))
+#y=int(input("Enter second number:"))
+#print("Addition is:",x+y)
+
+r=float(input("Enter a radius:"))
+pi=3.14
+result=pi*r*r
+print("Area of circle is:",result)
