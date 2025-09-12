ab2e08289aaed56696d74e813019cbd0467fbd24
ASHWINPATIDAR18 <ashwinpatidar2000@gmail.com>
2025-09-12 16:32:06 +0530

    Create vsthirdcode.py

---
+++ vsthirdcode.py
@@ -0,0 +1,35 @@
+'''h="vikas"
+for k in h:
+    print("Ram")
+    print("Shyam")'''
+
+#for i in range(5):
+#    print("Hello")
+
+'''x=[11,33,43,65,76,89,99]
+for i in x:
+    print(i)'''
+
+'''y="raju"
+for i in y:
+    
+    print(i,end='  ')'''
+
+'''x=0
+while x<5:
+    print(x)
+    x=x+1'''
+
+#WAPP to find max of 3 numbers
+x=int(input("Enter first number:"))
+y=int(input("Enter second number:"))
+z=int(input("Enter third number:"))
+
+if x>y and x>z:
+    print(x,"is Greater")
+elif y>x and y>z:
+    print(y,"is Greater")
+elif z>x and z>y:
+    print(z,"is Greater")
+
+    
