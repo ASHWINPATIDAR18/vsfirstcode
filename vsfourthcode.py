e5ae9af9feb2d1b52df9abd9c387e6b9f3f3cb0b
ASHWINPATIDAR18 <ashwinpatidar2000@gmail.com>
2025-09-13 16:55:03 +0530

    Create vsfourthcode.py

---
+++ vsfourthcode.py
@@ -0,0 +1,42 @@
+#Swapping
+'''x=int(input("Enter first number:"))
+y=int(input("Enter second number:"))
+
+print("x=",x,"y=",y)
+
+temp=x
+y=x
+y=temp
+print("After Swapping:")
+print("x=",x,"y=",y)'''
+
+#WAPP to check whether the given number is even or odd
+#x=int(input("Enter a number:"))
+#if x%2==0:
+#    print("It is even")
+#else:
+#    print("It is odd")
+
+#Adding new integer in list
+'''g1=[11,22,33,44,79.4,"ajay",6+7j]
+g1.append(64)
+for i in g1:
+    print(i)'''
+
+#slicing operator
+'''h1=[6,4,7,54,32,67,98.5,66,13]
+#p=h1[2:7:1]
+#p=h1[2:8:2]
+#p=h1[-1]
+p=h1[-1::-1]
+print(p)'''
+
+x="abhishek"
+#print(x[0])
+#print(x[1:5:])
+print(x[-1::-1])
+
+
+
+
+
